aa90e20d9922352ec39667c47d7367115e8df147
dawcas <dawcas@disroot.org>
2020-09-22 20:16:01 +0200

    ramas

---
+++ prueba.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="es">
+    <head>
+        <meta charset="utf-8" />
+        <title>Hola mundo</title>
+        <meta name="generator" content="Geany 1.33" />
+    </head>
+    <body>
+<?
+    echo "<p>Hola mundo</p>"
+?>
+    </body>
+</html>
